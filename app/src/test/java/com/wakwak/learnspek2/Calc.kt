bd2e37992db1f8656a65d7de2984001b78137476
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2018-08-18 16:20:28 +0900

    Tweak

---
+++ app/src/test/java/com/wakwak/learnspek2/Calc.kt
@@ -0,0 +1,6 @@
+package com.wakwak.learnspek2
+
+class Calc {
+
+    fun addition(i: Int, i2: Int): Int = i + i2
+}
---
+++ app/src/test/java/com/wakwak/learnspek2/ExampleSpecTest.kt
@@ -0,0 +1,20 @@
+package com.wakwak.learnspek2
+
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertEquals
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+object ExampleSpecTest : Spek({
+    describe("A calculation") {
+        val calc by memoized { Calc() }
+
+        it("should return 4") {
+            assertEquals(4, calc.addition(2, 2))
+        }
+    }
+})
