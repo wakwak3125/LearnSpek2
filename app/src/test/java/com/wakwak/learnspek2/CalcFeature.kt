c6795d40deeb26d06ca03ae446d2b16addef0056
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2018-08-20 12:57:38 +0900

    Add CalcFeature

---
+++ app/src/test/java/com/wakwak/learnspek2/CalcFeature.kt
@@ -0,0 +1,33 @@
+package com.wakwak.learnspek2
+
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
+import kotlin.test.assertEquals
+
+object CalcFeature : Spek({
+    Feature("Addition") {
+        val calc = Calc()
+        var result = 0
+
+        Scenario("Simple calculation") {
+
+            When("Add 2 + 2") {
+                result = calc.addition(2, 2)
+            }
+
+            Then("it should be 4") {
+                assertEquals(4, result)
+            }
+        }
+
+        Scenario("Simple calculation") {
+            When("Add -1 + -1") {
+                result = calc.addition(-1, -1)
+            }
+
+            Then("it should be -2") {
+                assertEquals(-2, result)
+            }
+        }
+    }
+})
